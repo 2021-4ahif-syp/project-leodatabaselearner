fdc71aba994b3e7547aa3a75f0e9bb52560b8f40
Test <superman.batboy@outlook.com>
2021-03-08 14:19:52 +0100

    Entities configured

--- src/main/java/at/htl/entity/Sql_Answer.java
+++ src/main/java/at/htl/entity/Sql_Answer.java
@@ -1,13 +1,23 @@
 package at.htl.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Sql_Answer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private boolean isCorr;
     private String answer;
 
+
+    public Sql_Answer() {
+    }
 
     public Sql_Answer(boolean isCorr, String answer) {
         this.isCorr = isCorr;
---
+++ src/main/java/at/htl/entity/Sql_Question.java
@@ -0,0 +1,58 @@
+package at.htl.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Sql_Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name ="TEXT")
+    String text;
+    @Column(name ="POINTS")
+    int points;
+    @Column(name ="DATE")
+    Date createdDate;
+
+
+    public Sql_Question() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
