1136d7803dba8c181a31fbc548b0a37459341156
Test <superman.batboy@outlook.com>
2021-06-14 15:27:04 +0200

    changes(constructor)

--- src/main/java/at/htl/entity/Sql_Topic.java
+++ src/main/java/at/htl/entity/Sql_Topic.java
@@ -2,5 +2,10 @@ package at.htl.entity;
 
 public class Sql_Topic {
     private Long id;
+    private String tipName;
 
+
+    public Sql_Topic(String tipName) {
+        this.tipName = tipName;
+    }
 }
