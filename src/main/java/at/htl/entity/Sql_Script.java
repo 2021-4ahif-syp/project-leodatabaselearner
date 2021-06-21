7392beff6d721bdd16a6d2288d84d4b2b9f30e14
Paatz <patrick.spisak@gmx.at>
2021-06-21 13:10:45 +0200

    Furhther changes

---
+++ src/main/java/at/htl/entity/Sql_Script.java
@@ -0,0 +1,9 @@
+package at.htl.entity;
+
+public class Sql_Script {
+    private long script_id;
+    private String script_type;
+    private String script_path;
+    private int script_order;
+    private long dm_id;
+}
