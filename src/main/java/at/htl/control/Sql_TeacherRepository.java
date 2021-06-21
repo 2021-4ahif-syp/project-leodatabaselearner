5df346da3bc4b56f7ef4f60890504532a26bd033
Paatz <patrick.spisak@gmx.at>
2021-06-21 15:00:18 +0200

    Repo

--- src/main/java/at/htl/control/Sql_TeacherRepository.java
+++ src/main/java/at/htl/control/Sql_TeacherRepository.java
@@ -1,5 +1,6 @@
 package at.htl.control;
 
+import at.htl.entity.Sql_Statement;
 import at.htl.entity.Sql_Teacher;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
@@ -7,4 +8,8 @@ import javax.transaction.Transactional;
 
 @Transactional
 public class Sql_TeacherRepository implements PanacheRepository<Sql_Teacher> {
+    public Sql_Teacher save(Sql_Teacher sqlTeacherToSave) {
+        return getEntityManager().merge(sqlTeacherToSave);
+    }
+
 }
