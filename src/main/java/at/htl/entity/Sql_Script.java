ff0319436b989be0bbb13a3626e00b95f599df6b
Paatz <patrick.spisak@gmx.at>
2021-06-21 13:28:42 +0200

    Furhther changes

--- src/main/java/at/htl/entity/Sql_Script.java
+++ src/main/java/at/htl/entity/Sql_Script.java
@@ -6,4 +6,30 @@ public class Sql_Script {
     private String script_path;
     private int script_order;
     private long dm_id;
+
+
+
+    public String getScript_type() {
+        return script_type;
+    }
+
+    public void setScript_type(String script_type) {
+        this.script_type = script_type;
+    }
+
+    public String getScript_path() {
+        return script_path;
+    }
+
+    public void setScript_path(String script_path) {
+        this.script_path = script_path;
+    }
+
+    public int getScript_order() {
+        return script_order;
+    }
+
+    public void setScript_order(int script_order) {
+        this.script_order = script_order;
+    }
 }
