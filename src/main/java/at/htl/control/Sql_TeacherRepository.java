1e1f1f17d51a09da0614080faa5188d317705729
Paatz <patrick.spisak@gmx.at>
2021-06-21 14:59:34 +0200

    Repo

--- src/main/java/at/htl/control/Sql_TeacherRepository.java
+++ src/main/java/at/htl/control/Sql_TeacherRepository.java
@@ -1,7 +1,10 @@
 package at.htl.control;
 
+import at.htl.entity.Sql_Teacher;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
 import javax.transaction.Transactional;
 
 @Transactional
-public class Sql_TeacherRepository {
+public class Sql_TeacherRepository implements PanacheRepository<Sql_Teacher> {
 }
