02e042e17b1f700617ed3e690c6f38a87a7f7dce
Paatz <patrick.spisak@gmx.at>
2021-06-21 13:35:42 +0200

    Furhther changes

--- src/main/java/at/htl/boundary/Sql_AnswerResource.java
+++ src/main/java/at/htl/boundary/Sql_AnswerResource.java
@@ -1,4 +1,27 @@
 package at.htl.boundary;
 
+import at.htl.control.Sql_AnswerRepository;
+import at.htl.control.Sql_ExerciseRepository;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@RequestScoped
+@Path("/Answer")
 public class Sql_AnswerResource {
+
+    @Inject
+    Sql_AnswerRepository sql_answerRepository;
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response findAll(){
+        return Response.ok(sql_answerRepository.findAll()).build();
+    }
+
 }
